8527ee2412494f0a28cacf435cf1a035e650dc51
Kay-Uwe Janssen <k.janssen@herbrand.de>
2017-09-14 11:03:46 +0200

    Mergee AbstractComponentElementProcessor into ComponentNamedElementProcessor

--- src/main/java/de/morphbit/thymeleaf/processor/ComponentNamedElementProcessor.java
+++ src/main/java/de/morphbit/thymeleaf/processor/ComponentNamedElementProcessor.java
@@ -1,52 +1,138 @@
 package de.morphbit.thymeleaf.processor;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
+import de.morphbit.thymeleaf.helper.FragmentHelper;
+import de.morphbit.thymeleaf.helper.WithHelper;
 import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.model.IAttribute;
+import org.thymeleaf.model.IElementTag;
 import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.model.ITemplateEvent;
+import org.thymeleaf.processor.element.AbstractElementModelProcessor;
 import org.thymeleaf.processor.element.IElementModelStructureHandler;
+import org.thymeleaf.templatemode.TemplateMode;
 
-import de.morphbit.thymeleaf.helper.FragmentHelper;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
-public class ComponentNamedElementProcessor extends AbstractComponentElementProcessor {
+import static java.util.Collections.singleton;
 
-	private static final int PRECEDENCE = 75;
+public class ComponentNamedElementProcessor extends AbstractElementModelProcessor {
 
-	private final String fragmentName;
-	private final Set<String> excludeAttributes = new HashSet<>(); 
+    private static final String THYMELEAF_FRAGMENT_PREFIX = "th";
+    private static final String THYMELEAF_FRAGMENT_ATTRIBUTE = "fragment";
+    private static final String REPLACE_CONTENT_TAG = "tc:content";
+    private static final String DYNAMIC_ATT_PREFIX = "th:";
 
-	public ComponentNamedElementProcessor(final String dialectPrefix, final String tagName, final String fragmentName) {
-		super(dialectPrefix, tagName, PRECEDENCE);
-		this.fragmentName = fragmentName;
-		excludeAttributes.add("params");
-	}
+    private static final int PRECEDENCE = 350;
 
-	@Override
-	protected void doProcess(ITemplateContext context, IModel model, IElementModelStructureHandler structureHandler) {
+    private final Set<String> excludeAttributes = singleton("params");
+    private final String fragmentName;
 
-		IProcessableElementTag tag = processElementTag(context, model);
-		Map<String, String> attrMap = processAttribute(context, tag);
+    public ComponentNamedElementProcessor(final String dialectPrefix, final String tagName, final String fragmentName) {
+        super(TemplateMode.HTML, dialectPrefix, tagName, true, null, false, PRECEDENCE);
+        this.fragmentName = fragmentName;
+    }
 
-		String param = attrMap.get("params");
+    @Override
+    protected void doProcess(ITemplateContext context, IModel model, IElementModelStructureHandler structureHandler) {
+        IProcessableElementTag tag = processElementTag(context, model);
+        Map<String, String> attrMap = processAttribute(context, tag);
 
-		IModel base = model.cloneModel();
-		base.remove(0);
-		
-		if(base.size() > 1) {
-			base.remove(base.size() - 1);
-		}
+        String param = attrMap.get("params");
 
-		IModel frag = FragmentHelper.getFragmentModel(context, fragmentName + (param == null ? "" : "(" + param + ")"),
-				structureHandler, THYMELEAF_FRAGMENT_PREFIX, THYMELEAF_FRAGMENT_ATTRIBUTE);
+        IModel base = model.cloneModel();
+        base.remove(0);
 
-		model.reset();
-		model.addModel(mergeModel(frag, base, REPLACE_CONTENT_TAG));
-		
-		processVariables(attrMap, context, structureHandler, excludeAttributes);
-		
-	}
+        if (base.size() > 1) {
+            base.remove(base.size() - 1);
+        }
 
+        IModel frag = FragmentHelper.getFragmentModel(context, fragmentName + (param == null ? "" : "(" + param + ")"),
+                structureHandler, THYMELEAF_FRAGMENT_PREFIX, THYMELEAF_FRAGMENT_ATTRIBUTE);
+
+        model.reset();
+        model.addModel(mergeModel(frag, base, REPLACE_CONTENT_TAG));
+
+        processVariables(attrMap, context, structureHandler, excludeAttributes);
+    }
+
+    private IProcessableElementTag processElementTag(ITemplateContext context, IModel model) {
+        ITemplateEvent firstEvent = model.get(0);
+        for (IProcessableElementTag tag : context.getElementStack()) {
+            if (locationMatches(firstEvent, tag)) {
+                return tag;
+            }
+        }
+        return null;
+    }
+
+    private boolean locationMatches(ITemplateEvent a, ITemplateEvent b) {
+        return Objects.equals(a.getTemplateName(), b.getTemplateName())
+                && Objects.equals(a.getLine(), b.getLine())
+                && Objects.equals(a.getCol(), b.getCol());
+    }
+
+    private void processVariables(Map<String, String> attrMap, ITemplateContext context,
+                                    IElementModelStructureHandler structureHandler, Set<String> excludeAttr) {
+        for (Map.Entry<String, String> entry : attrMap.entrySet()) {
+            if (excludeAttr.contains(entry.getKey()) || entry.getKey().startsWith(this.getDialectPrefix() + ":")) {
+                continue;
+            }
+            WithHelper.processWith(context, entry.getKey() + "=" + entry.getValue(), structureHandler);
+        }
+    }
+
+    private Map<String, String> processAttribute(final ITemplateContext context, IProcessableElementTag tag) {
+        Map<String, String> attMap = new HashMap<>();
+        for (final IAttribute attribute : tag.getAllAttributes()) {
+            String completeName = attribute.getAttributeCompleteName();
+            if (!completeName.startsWith(DYNAMIC_ATT_PREFIX)) {
+                attMap.put(completeName, attribute.getValue());
+            }
+        }
+
+        return attMap;
+    }
+
+    private IModel mergeModel(IModel fragment, IModel body, final String replaceTag) {
+        IModel mergedModel = insert(fragment, body, replaceTag);
+        mergedModel = remove(mergedModel, replaceTag);
+        mergedModel = remove(mergedModel, replaceTag);
+        return mergedModel;
+    }
+
+    private IModel insert(IModel fragment, IModel body, final String replaceTag) {
+        IModel mergedModel = fragment.cloneModel();
+        int size = mergedModel.size();
+        ITemplateEvent event = null;
+        for (int i = 0; i < size; i++) {
+            event = mergedModel.get(i);
+            if (event instanceof IElementTag) {
+                if (event.toString().contains(replaceTag)) {
+                    mergedModel.insertModel(i, body);
+                    break;
+                }
+            }
+        }
+        return mergedModel;
+    }
+
+    private IModel remove(IModel fragment, final String replaceTag) {
+        IModel mergedModel = fragment.cloneModel();
+        int size = mergedModel.size();
+        ITemplateEvent event = null;
+        for (int i = 0; i < size; i++) {
+            event = mergedModel.get(i);
+            if (event instanceof IElementTag) {
+                if (event.toString().contains(replaceTag)) {
+                    mergedModel.remove(i);
+                    break;
+                }
+            }
+        }
+        return mergedModel;
+    }
 }
