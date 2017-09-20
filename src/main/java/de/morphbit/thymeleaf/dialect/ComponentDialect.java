3b881c794861e0308edd0ff0503b3e5ae7a07334
Danny Rottstegge <d.rottstegge@evosec.de>
2017-09-20 11:27:29 +0200

    Add possibility to register parser

--- src/main/java/de/morphbit/thymeleaf/dialect/ComponentDialect.java
+++ src/main/java/de/morphbit/thymeleaf/dialect/ComponentDialect.java
@@ -1,15 +1,17 @@
 package de.morphbit.thymeleaf.dialect;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.thymeleaf.dialect.AbstractProcessorDialect;
 import org.thymeleaf.processor.IProcessor;
-import org.thymeleaf.standard.StandardDialect;
 
 import de.morphbit.thymeleaf.model.ThymeleafComponent;
 import de.morphbit.thymeleaf.processor.ComponentNamedElementProcessor;
 import de.morphbit.thymeleaf.processor.OnceAttributeTagProcessor;
+import de.morphbit.thymeleaf.service.IThymeleafComponentParser;
 
 public class ComponentDialect extends AbstractProcessorDialect {
 
@@ -18,6 +20,7 @@ public class ComponentDialect extends AbstractProcessorDialect {
 	public static final int PRECEDENCE = 1000;
 
 	private final Set<ThymeleafComponent> components;
+	private final List<IThymeleafComponentParser> parsers = new ArrayList<>();
 
 	public ComponentDialect() {
 		this(null);
@@ -39,7 +42,25 @@ public class ComponentDialect extends AbstractProcessorDialect {
 				    comp.getName(), comp.getFragmentTemplate()));
 			}
 		}
+
+		for (ThymeleafComponent comp : parseComponents()) {
+			processors.add(new ComponentNamedElementProcessor(dialectPrefix,
+			    comp.getName(), comp.getFragmentTemplate()));
+		}
+
 		return processors;
 	}
 
+	private Set<ThymeleafComponent> parseComponents() {
+		Set<ThymeleafComponent> parsedComponents = new HashSet<>();
+		for (IThymeleafComponentParser parser : this.parsers) {
+			parsedComponents.addAll(parser.parse());
+		}
+
+		return parsedComponents;
+	}
+
+	public void addParser(IThymeleafComponentParser parser) {
+		this.parsers.add(parser);
+	}
 }
