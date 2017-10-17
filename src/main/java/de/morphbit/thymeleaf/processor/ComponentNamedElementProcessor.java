01022bb797afcb29b4a87ff9429a8bfeb2c87ab5
Danny Rottstegge <d.rottstegge@evosec.de>
2017-10-17 16:19:17 +0200

    Add possibility to have replaceable texts

--- src/main/java/de/morphbit/thymeleaf/processor/ComponentNamedElementProcessor.java
+++ src/main/java/de/morphbit/thymeleaf/processor/ComponentNamedElementProcessor.java
@@ -22,11 +22,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.engine.AttributeNames;
 import org.thymeleaf.model.IAttribute;
 import org.thymeleaf.model.IElementTag;
 import org.thymeleaf.model.IModel;
+import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.model.IProcessableElementTag;
 import org.thymeleaf.model.ITemplateEvent;
 import org.thymeleaf.processor.element.AbstractElementModelProcessor;
@@ -85,7 +89,9 @@ public class ComponentNamedElementProcessor
 		    structureHandler, StandardDialect.PREFIX, FRAGMENT_ATTRIBUTE);
 
 		model.reset();
-		model.addModel(mergeModels(frag, base, REPLACE_CONTENT_TAG));
+
+		IModel replaced = replaceAllAttributeValues(attrMap, context, frag);
+		model.addModel(mergeModels(replaced, base, REPLACE_CONTENT_TAG));
 
 		processVariables(attrMap, context, structureHandler, excludeAttributes);
 	}
@@ -180,5 +186,82 @@ public class ComponentNamedElementProcessor
 			}
 		}
 		return -1;
+	}
+
+	private IModel replaceAllAttributeValues(Map<String, String> attributes,
+	        ITemplateContext context, IModel model) {
+		Map<String, String> replaceAttributes = findAllAttributesStartsWith(
+		    attributes, super.getDialectPrefix(), "repl-", true);
+
+		if (replaceAttributes.isEmpty()) {
+			return model;
+		}
+		IModel clonedModel = model.cloneModel();
+		int size = model.size();
+		for (int i = 0; i < size; i++) {
+			ITemplateEvent replacedEvent = replaceAttributeValue(context,
+			    clonedModel.get(i), replaceAttributes);
+			if (replacedEvent != null) {
+				clonedModel.replace(i, replacedEvent);
+			}
+
+		}
+		return clonedModel;
+	}
+
+	private ITemplateEvent replaceAttributeValue(ITemplateContext context,
+	        ITemplateEvent model, Map<String, String> replaceValueMap) {
+		IProcessableElementTag firstEvent = null;
+		if (!replaceValueMap.isEmpty()
+		        && model instanceof IProcessableElementTag) {
+			final IModelFactory modelFactory = context.getModelFactory();
+
+			firstEvent = (IProcessableElementTag) model;
+			for (Map.Entry<String, String> entry : firstEvent.getAttributeMap()
+			    .entrySet()) {
+				String oldAttrValue = entry.getValue();
+				String replacePart = getReplaceAttributePart(oldAttrValue);
+				if (replacePart != null
+				        && replaceValueMap.containsKey(replacePart)) {
+					String newStringValue =
+					        oldAttrValue.replace("--:" + replacePart + ":--",
+					            replaceValueMap.get(replacePart));
+					firstEvent = modelFactory.replaceAttribute(firstEvent,
+					    AttributeNames.forTextName(entry.getKey()),
+					    entry.getKey(), newStringValue);
+				}
+			}
+		}
+		return firstEvent;
+	}
+
+	private Map<String, String> findAllAttributesStartsWith(
+	        final Map<String, String> attributes, final String prefix,
+	        final String attributeName, boolean removeStart) {
+		Map<String, String> matchingAttributes = new HashMap<>();
+		for (Map.Entry<String, String> entry : attributes.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			if (key.startsWith(prefix + ":" + attributeName)
+			        || key.startsWith("data-" + prefix + "-" + attributeName)) {
+				if (removeStart) {
+					key = key.replaceAll("^" + prefix + ":" + attributeName,
+					    "");
+					key = key.replaceAll(
+					    "^data-" + prefix + "-" + attributeName, "");
+				}
+				matchingAttributes.put(key, value);
+			}
+		}
+		return matchingAttributes;
+	}
+
+	private String getReplaceAttributePart(String attributeValue) {
+		Pattern pattern = Pattern.compile(".*--:(.*):--.*");
+		Matcher matcher = pattern.matcher(attributeValue);
+		while (matcher.find()) {
+			return matcher.group(1);
+		}
+		return null;
 	}
 }
