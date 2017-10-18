cfbe8f121f3f7f7019ea2463eb24efb1f1c9082a
Danny Rottstegge <d.rottstegge@evosec.de>
2017-10-18 16:25:33 +0200

    Complete configuration

--- src/test/java/de/morphbit/thymeleaf/base/AbstractThymeleafComponentDialectTest.java
+++ src/test/java/de/morphbit/thymeleaf/base/AbstractThymeleafComponentDialectTest.java
@@ -2,12 +2,16 @@ package de.morphbit.thymeleaf.base;
 
 import org.junit.BeforeClass;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import de.morphbit.thymeleaf.dialect.ComponentDialect;
+import de.morphbit.thymeleaf.parser.StandardThymeleafComponentParser;
 
 public class AbstractThymeleafComponentDialectTest {
+
+	private static final String BASE_PATH = "templates";
 
 	private static TemplateEngine templateEngine;
 
@@ -16,16 +20,25 @@ public class AbstractThymeleafComponentDialectTest {
 		setupThymeleaf();
 	}
 
+	protected String processThymeleafFile(String fileName, Context context) {
+		return templateEngine.process(BASE_PATH + "/" + fileName, context);
+	}
+
 	private static void setupThymeleaf() {
 		ClassLoaderTemplateResolver templateResolver =
 		        new ClassLoaderTemplateResolver();
 		templateResolver.setCacheable(false);
 		templateResolver.setCharacterEncoding("UTF-8");
 		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setSuffix(".html");
 
 		templateEngine = new TemplateEngine();
 		templateEngine.setTemplateResolver(templateResolver);
 		final ComponentDialect dialect = new ComponentDialect();
+		final StandardThymeleafComponentParser parser =
+		        new StandardThymeleafComponentParser("", ".html",
+		            "templates/components");
+		dialect.addParser(parser);
 		templateEngine.addDialect(dialect.getPrefix(), dialect);
 	}
 }
