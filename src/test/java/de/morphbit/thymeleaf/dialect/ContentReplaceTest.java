2c2e6cef72351de0067c89772c26c9dfa249e464
Danny Rottstegge <d.rottstegge@evosec.de>
2017-10-18 17:24:43 +0200

    Add test

--- src/test/java/de/morphbit/thymeleaf/dialect/ContentReplaceTest.java
+++ src/test/java/de/morphbit/thymeleaf/dialect/ContentReplaceTest.java
@@ -10,16 +10,32 @@ import de.morphbit.thymeleaf.base.AbstractThymeleafComponentDialectTest;
 
 public class ContentReplaceTest extends AbstractThymeleafComponentDialectTest {
 
-	private static final String FILE = "link_with_content.html";
+	private static final String FILE_LINK_WITH_CONTENT =
+	        "link_with_content.html";
+	private static final String FILE_LINK_WITH_CONTENT_AND_VARIABLE =
+	        "link_with_content_and_variable.html";
 
 	@Test
 	public void itShouldNotRenderNamespaceTagsAndReplaceContent() {
-		String html = processThymeleafFile(FILE, new Context());
+		String html =
+		        processThymeleafFile(FILE_LINK_WITH_CONTENT, new Context());
 
 		assertNotNull(html);
 		assertTrue(!html.contains("tc:link"));
 		assertTrue(!html.contains("tc:content"));
 		assertTrue(html.contains("<a href=\"http://www.test.com\">"));
 		assertTrue(html.contains("<span>Test</span>"));
+		assertTrue(!html.contains("<span>&gt;&gt; </span>"));
+	}
+
+	@Test
+	public void itShouldNotRenderNamespaceTagsAndReplaceContentAndConsiderVariable() {
+		String html = processThymeleafFile(FILE_LINK_WITH_CONTENT_AND_VARIABLE,
+		    new Context());
+
+		assertNotNull(html);
+		assertTrue(html.contains("<a href=\"http://www.test.com\">"));
+		assertTrue(html.contains("<span>Test</span>"));
+		assertTrue(html.contains("<span>&gt;&gt; </span>"));
 	}
 }
